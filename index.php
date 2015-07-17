3552a7a7e90e8cdce89e2d31d9b218d2d28dbd9f
muzeum2 <muzeum@o2.pl>
2015-07-18 00:20:15 +0200

    2015.07.18
    początek, kompletowanie bibliotek i tworzenie ogólnych wymagań dla
    projektu

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Robbery - gra przeglądarkowa
+ * autor Tomasz Muzia
+ * projekt powstaje do nauki programowania obiektowego i na na celu stworzenie prostej gry przeglądarkowej
+ * w projekcie są używane biblioteki opensource.
+ *  
+ */
+
+
